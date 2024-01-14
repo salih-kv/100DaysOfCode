dcf62fe0db00ad752d4330dbc7d6561631db9b85
salih-kv <profileofsalih@gmail.com>
2024-01-14 20:33:27 +0530

    cookies

--- Day 11 - 20/Day 14/index.js
+++ Day 11 - 20/Day 14/index.js
@@ -1,11 +1,18 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import routes from "./routes/index.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cookieParser("secret-msg"));
 app.use(routes);
+
+app.get("/", (req, res) => {
+  res.cookie("msg", "hello world", { maxAge: 60000, signed: true }); // 1m
+  res.send("Hello World");
+});
 
 /** */
 app.listen(PORT, () => {
--- Day 11 - 20/Day 14/routes/products.js
+++ Day 11 - 20/Day 14/routes/products.js
@@ -2,8 +2,14 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("api/products", (req, res) => {
-  res.send("[]");
+router.get("/api/products", (req, res) => {
+  console.log(req.signedCookies);
+
+  if (req.signedCookies && req.signedCookies.msg === "hello-world") {
+    res.send([{ id: 1, productName: "Laptop x", price: 60000 }]);
+  }
+
+  res.send("Sorry. You need the correct cookie");
 });
 
 export default router;
